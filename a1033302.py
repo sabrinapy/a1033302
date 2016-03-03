d43961dde178196e74ddedd1e8409c1a87749d6f
Sabrina <sabrina74483@gmail.com>
2016-03-03 11:52:00 +0800

    comment

--- a1033302.py
+++ a1033302.py
@@ -1,6 +1,4 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-print '資管107'
-print 'A1033302'
-print '劉又禎'
+print 'python is fun'
--- me.py
+++ me.py
@@ -1,2 +1,6 @@
-print "IM107\n";
-print "A1033302\n";
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+print '資管107'
+print 'A1033302'
+print '劉又禎'
